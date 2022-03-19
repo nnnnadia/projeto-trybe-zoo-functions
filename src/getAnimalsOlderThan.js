253bb90ebbcaa856bb3f8015f61b1efd6fa06694
nnnnadia <tristaonadia@gmail.com>
2022-03-19 19:27:30 -0300

    Requisito 2 feito.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+/* 
+  getAnimalsOlderThan(animal, age)
+  -cria a variável animals com o objeto de um 'animal'
+  -retorna se todos os animais residentes são mais velhos que 'age'
+*/
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = data.species.find(specie => specie.name === animal);
+  return animals.residents.every(animal => animal.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
