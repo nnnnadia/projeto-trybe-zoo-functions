2872b16759b3414425bae3288b46b47c8dd2e6eb
nnnnadia <tristaonadia@gmail.com>
2022-03-22 11:23:58 -0300

    Requisito 1 feito.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+// Input: String (recebido(s) como parâmetro(s))
+// - '0938aa23-f153-4937-9f88-4858b24d6bce'
+// - '0938aa23-f153-4937-9f88-4858b24d6bce', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'
+
+// Output: Array de Objetos
+// [
+//   {
+//     id: input,
+//     ...
+//   },
+//   {
+//     id: input,
+//     ...
+//   },
+// ]
+
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => data.species.find((animalObj) => animalObj.id === id));
 }
 
 module.exports = getSpeciesByIds;
