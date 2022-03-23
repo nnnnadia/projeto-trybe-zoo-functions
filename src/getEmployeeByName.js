3a4acbfa4f64046bf8044b6dc0568c529aa8479f
nnnnadia <tristaonadia@gmail.com>
2022-03-22 22:42:06 -0300

    Requisito 3 - Adiciona comentários.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,5 +1,9 @@
 const { employees } = require('../data/zoo_data');
 
+/*
+  getEmployeeByName retorna o objeto do empregado quando o valor recebido é o seu primeiro ou último nome e retorna um objeto vazio quando não recebe nada.
+*/
+
 function getEmployeeByName(empName) {
   if (empName !== undefined) {
     return employees.find((emp) => emp.firstName === empName || emp.lastName === empName);
