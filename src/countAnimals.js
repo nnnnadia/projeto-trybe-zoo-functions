d40d3373c64f9c0fcc231c05a9b25d50a8a1e8bf
nnnnadia <tristaonadia@gmail.com>
2022-03-23 09:51:49 -0300

    Requisito 5 - countAnimals retorna o número de residentes de uma espécie e sexo específicos.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,16 @@
 const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  return species
-    .find((specie) => specie.name === animal.specie).residents.length;
+  if (animal.specie !== undefined) {
+    const residents = species
+      .find((specie) => specie.name === animal.specie).residents;
+    if (animal.sex !== undefined) {
+      return residents
+        .reduce((acc, curr) => (curr.sex === animal.sex ? acc += 1 : acc), 0);
+    }
+    return residents.length;
+  }
 }
 
 module.exports = countAnimals;
+ 
