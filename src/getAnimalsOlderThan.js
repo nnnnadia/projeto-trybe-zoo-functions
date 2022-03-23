305a81218bd223cba16f045a8330e0a1ca4b3d67
nnnnadia <tristaonadia@gmail.com>
2022-03-22 21:16:31 -0300

    Correções do ESLint.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,13 +1,13 @@
 const data = require('../data/zoo_data');
 
-/* 
+/*
   getAnimalsOlderThan(animal, age)
   -cria a variável animals com o objeto de um 'animal'
   -retorna se todos os animais residentes são mais velhos que 'age'
 */
 function getAnimalsOlderThan(animal, age) {
-  const animals = data.species.find(specie => specie.name === animal);
-  return animals.residents.every(animal => animal.age >= age);
+  const animals = data.species.find((specie) => specie.name === animal);
+  return animals.residents.every((resident) => resident.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(empName) {
+  return employees.find((emp) => emp.firstName === empName || emp.lastName === empName);
 }
 
 module.exports = getEmployeeByName;
