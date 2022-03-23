a3ab905fd302556bad14b290c67d6effb6a3cc17
nnnnadia <tristaonadia@gmail.com>
2022-03-23 11:43:35 -0300

    Requisito 5 - Adiciona comentários.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,11 +1,35 @@
 const { species } = require('../data/zoo_data');
 
+/*
+  Possíveis entradas de countAnimals:
+    ():
+      {
+        species.name: species.residents.length,
+        ...
+      }
+    ({ animal.specie }):
+      specie.residents.length
+    ({ animal.specie, animal.sex }):
+      specie.residents.reduce(sex)
+*/
+
+/*
+  countAllSpecies cria um objeto cujas chaves são os nomes de todas as espécies e os valores são o total de residentes.
+
+  referência utilizada:
+  https://dev.to/_bigblind/quick-tip-transform-an-array-into-an-object-using-reduce-2gh6
+*/
+
 function countAllSpecies() {
   return species.reduce((acc, specie) => {
     const { name, residents } = specie;
     return { ...acc, [name]: residents.length };
   }, {});
 }
+
+/*
+  countAnimals testa se houve alguma entrada e retorna o número total de residentes ou o número total de residentes de um determinado sexo, senão chama countAllSpecies.
+*/
 
 function countAnimals(animal) {
   if (animal === undefined) {
