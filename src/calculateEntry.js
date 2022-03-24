1f3c603231f171ffc709e409fd04e0a8ff9fcb29
nnnnadia <tristaonadia@gmail.com>
2022-03-24 17:05:30 -0300

    Requisito 6 - Adiciona comentários.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,5 +1,9 @@
 const { prices } = require('../data/zoo_data');
 
+/*
+  countEntrants recebe uma lista de objetos com a idade dos visitantes e retorna um objeto com a quantidade de pessoas em cada faixa etária.
+*/
+
 function countEntrants(entrants) {
   return entrants
     .reduce((acc, { age }) => {
@@ -16,6 +20,19 @@ function countEntrants(entrants) {
       senior: 0,
     });
 }
+
+/*
+  calculateEntry confere se os valores entrados são válidos, senão 0 é retornado.
+  É acumulado a multiplicação da quantidade de pessoas em uma faixa etária pelo valor que a faixa etária paga.
+  Como a saída de countEntrants devolve a mesma estrutura em que os dados dos preços estão, o acesso ocorre da seguinte forma:
+
+  countEntrants(a):  prices(b):         para cada elemento será feito:
+  [                  {                  a[1] * b[a[0]] + acc
+    ['adult', 1],      adult: 49.99,    1 * b['adult'] + 0
+    ['child', 2],      senior: 24.99,   1 * 49.99 + 0
+    ['senior', 0],     child: 20.99,    49.99
+  ]                  }
+*/
 
 function calculateEntry(entrants) {
   if (entrants === undefined || entrants.length === undefined) return 0;
