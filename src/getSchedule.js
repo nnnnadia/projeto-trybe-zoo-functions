71d984eb160bd9bb5cc669b804e583b48107caac
nnnnadia <tristaonadia@gmail.com>
2022-03-24 19:34:16 -0300

    Requisito 8 - getSchedule retorna o expediente de um único dia.

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,26 @@
-const data = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
+
+const weekDays = Object.keys(hours);
+
+function createDayObj(day) {
+  const { open, close } = hours[day];
+  const avAnimals = species
+    .filter(({ availability }) => availability.includes(day))
+    .map((spec) => spec.name);
+  return ({
+    [day]: {
+      officeHour: `Open from ${open}am until ${close}pm`,
+      exhibition: avAnimals,
+    }
+  });
+}
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (Object.keys(hours).some((day) => day === scheduleTarget)) {
+    return createDayObj(scheduleTarget);
+  }
+  return hours[scheduleTarget];
+  // return createDayObj(scheduleTarget);
 }
 
 module.exports = getSchedule;
