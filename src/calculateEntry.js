94a342843d7fe5924454d3b46968fb7d2d166e70
nnnnadia <tristaonadia@gmail.com>
2022-03-24 15:57:44 -0300

    Requisito 6 - Implementação de calculateEntry.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  //  seu código aqui
+  return entrants
+    .reduce((acc, { age }) => {
+      if (age < 18) {
+        acc.child += 1;
+        return acc;
+      }
+      if (age < 50) {
+        acc.adult += 1;
+        return acc;
+      }
+      acc.senior += 1;
+      return acc;
+    }, {
+      adult: 0,
+      child: 0,
+      senior: 0,
+    });
 }
 
 function calculateEntry(entrants) {
