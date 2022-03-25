6910a006865fe272555a4347377c1a857318939b
nnnnadia <tristaonadia@gmail.com>
2022-03-25 10:29:57 -0300

    Requisito 10 - getEmployeesCoverage retorna a ficha de todos os funcionários quando não recebe nada. Implementação de getEmployeeRecord, getEmployeeFullname, getAnimalsNames e getAnimalsLocations.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,38 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getAnimalsLocations(specs) {
+  return species
+    .filter((spec) => specs.includes(spec.name))
+    .map((spec) => spec.location);
+}
+
+function getAnimalsNames(emp) {
+  return species
+    .filter((spec) => emp[0].responsibleFor.includes(spec.id))
+    .map((spec) => spec.name);
+}
+
+function getEmployeeFullname(emp) {
+  return `${emp[0].firstName} ${emp[0].lastName}`;
+}
+
+function getEmployeeRecord(emp) {
+  const curEmp = employees.filter(({ id }) => id === emp);
+  const responsibleSpecs = [...getAnimalsNames(curEmp)];
+  return ({
+    id: emp,
+    fullName: getEmployeeFullname(curEmp),
+    species: responsibleSpecs,
+    locations: getAnimalsLocations(responsibleSpecs),
+  });
+}
+
+function getEmployeesCoverage(token) {
+  if (token === undefined) {
+    return employees
+      .map((emp) => emp.id)
+      .map((emp) => getEmployeeRecord(emp));
+  }
 }
 
 module.exports = getEmployeesCoverage;
