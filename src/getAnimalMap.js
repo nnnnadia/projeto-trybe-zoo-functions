0d4f6f24ea0aef25e42b5caaba068f20993553d4
nnnnadia <tristaonadia@gmail.com>
2022-03-26 08:59:21 -0300

    Requisito 7 - getAnimalMap retorna os animais categorizados por localização.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,19 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const filterSpecieByLocation = (location) => {
+  return species
+    .filter((specie) => specie.location === location)
+    .map((specie) => specie.name);
+}
+
 
 function getAnimalMap(options) {
-  // seu código aqui
+  return ({
+    NE: filterSpecieByLocation('NE'),
+    NW: filterSpecieByLocation('NW'),
+    SE: filterSpecieByLocation('SE'),
+    SW: filterSpecieByLocation('SW'),
+  });
 }
 
 module.exports = getAnimalMap;
