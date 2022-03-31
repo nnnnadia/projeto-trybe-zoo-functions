65b215f4cf950cdf1009e42deca607b1b505db05
nnnnadia <tristaonadia@gmail.com>
2022-03-31 19:25:35 -0300

    Requisito 7 - Comenta e refatora código.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -4,6 +4,9 @@ let speciesLocations = {};
 let namedSpeciesLocations = {};
 const locationsList = ['NE', 'NW', 'SE', 'SW'];
 
+/*
+  sortNames acessa cada objeto em cada região e ordena alfabeticamente cada valor (lista de nomes) desses objetos.
+*/
 function sortNames() {
   locationsList.forEach((location) => {
     namedSpeciesLocations[location].forEach((obj) => {
@@ -13,7 +16,11 @@ function sortNames() {
   });
 }
 
-function getResidentsList(animal, sex) {
+/*
+  getNamesList cria um objeto em que animal é a chave e o valor é a lista de nomes.
+  A lista é gerada através da lista residents do zoo_data, ela pode ser filtrada por sexo e depois é modificada para conter apenas os nomes.
+*/
+function getNamesList(animal, sex) {
   const animalObj = {};
   animalObj[animal] = species
     .find((specie) => specie.name === animal).residents;
@@ -29,21 +36,28 @@ function getResidentsList(animal, sex) {
   return animalObj;
 }
 
-function transformNamesObject(location, sex) {
-  const animalsList = speciesLocations[location];
-  namedSpeciesLocations[location] = animalsList
-    .map((animal) => getResidentsList(animal, sex));
-}
-
+/*
+  generateNamesMap para cada região ele cria uma lista com os animais nela, em um novo objeto ele busca os nomes dos residentes.
+*/
 function generateNamesMap(sex) {
-  locationsList.forEach((location) => transformNamesObject(location, sex));
+  locationsList.forEach((location) => {
+    const animalsList = speciesLocations[location];
+    namedSpeciesLocations[location] = animalsList
+      .map((animal) => getNamesList(animal, sex));
+  });
   return namedSpeciesLocations;
 }
 
+/*
+  filterSpecieByLocation filtra os animais de uma região e retorna apenas os nomes das espécies.
+*/
 const filterSpecieByLocation = (location) => species
   .filter((specie) => specie.location === location)
   .map((specie) => specie.name);
 
+/*
+  generateDefaultMap cria o mapa com cada espécie de cada região.
+*/
 function generateDefaultMap() {
   speciesLocations = {
     NE: filterSpecieByLocation('NE'),
@@ -53,6 +67,11 @@ function generateDefaultMap() {
   };
 }
 
+/*
+  getAnimalMap cria um mapa padrão e o devolve caso nenhuma opção é informada.
+  Caso a opção sexo seja declarada um mapa com os nomes é gerado filtrados por sexo, senão é gerado sem o filtro.
+  Caso sorted seja verdadeiro as listas de nomes é ordenada alfabeticamente.
+*/
 function getAnimalMap(options) {
   generateDefaultMap();
   if (!options || !options.includeNames) {
