44e392ab1a0a88bea228daf8c22b4a89ee5f8067
nnnnadia <tristaonadia@gmail.com>
2022-04-01 10:27:58 -0300

    Requisito 9 - Comenta código.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,8 +1,14 @@
-const { species, employees } = require('../data/zoo_data');
-// const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data')
 
+/*
+  findById encontra um objeto com o id recebido. Nesse contexto where é species ou employees.
+*/
 const findById = (where, id) => where.find((item) => item.id === id);
 
+/*
+  getOldestFromFirstSpecies encontra a id da primeira espécie que o funcionário com a id recebida é responsável.
+  Depois a lista de residentes desta espécie é selecionada e a partir dela é verificado qual deles é o mais velho. Com esta informação é retornado uma lista com nome, sexo e idade.
+*/
 function getOldestFromFirstSpecies(id) {
   const firstSpeciesId = findById(employees, id).responsibleFor[0];
   const olderResidentObj = findById(species, firstSpeciesId).residents
